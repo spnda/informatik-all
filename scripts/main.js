37734b07998581b259d89c07a8b3591a863b18f4
sean <43609023+spnda@users.noreply.github.com>
2021-10-26 17:54:27 +0200

    Add cookies for storing the last visited page

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,35 @@
 const min = 0;
 const max = 3;
 
+if (Cookies.get("pageIndex") === undefined) {
+    Cookies.set("pageIndex", 0);
+}
+
 new Vue({
     el: '#app',
     data: {
         back: false,
         pageIndex: 0,
     },
+    mounted() {
+        let indx = Cookies.get('pageIndex');
+        if (indx !== undefined) {
+            this.pageIndex = parseInt(indx);
+        }
+    },
     methods: {
         previous() {
             if (this.pageIndex > min) {
                 this.back = true;
                 this.pageIndex--;
+                Cookies.set('pageIndex', this.pageIndex);
             }
         },
         next() {
             if (this.pageIndex < max) {
                 this.back = false;
                 this.pageIndex++;
+                Cookies.set('pageIndex', this.pageIndex);
             }
         },
     }
